f5386e2dd0c24c354afb8f2efd731e60283dd728
bibi1989 <bibiaremieye@gmail.com>
2020-05-26 18:22:02 +0100

    merge latest from master

--- src/services/AppService.ts
+++ src/services/AppService.ts
@@ -1,18 +1,11 @@
-import {observable, action} from 'mobx';
 import { FetchQl } from "../lib/client";
 import collectPaymentCall, {
   ICollectPayment,
 } from "../data/graphql/collectPayment";
-import loginCall from '../data/graphql/login';
-import {IAdmin} from '../data/models/Admin';
-
 
 export default class AppService {
   client: FetchQl;
   isLogedin: boolean = false;
-  @observable isLoading: boolean = false
-  admin: IAdmin | null = null;
-  errors = observable.array<string>([]);
 
   constructor(client: FetchQl) {
     this.client = client;
@@ -22,18 +15,8 @@ export default class AppService {
     await collectPaymentCall(this.client, data);
   };
 
-  @action
-  async login ({ email, password }: { email: string; password: string }) {
-    this.isLoading = true;
-    const {admin, hhh: accessToken, errors} = await loginCall(this.client, {email, password})
-    
-    if (admin && accessToken) {
-      this.isLogedin = true;
-      this.admin = admin;
-      localStorage.setItem('session', accessToken)
-    } else if (errors) {
-      this.errors = errors.map(({code, field}: any) => `${code}_${field}`);
-    }
-    this.isLoading = false;
+  login = ({ email, password }: { email: string; password: string }) => {
+    // pretend say u de login
+    this.isLogedin = true;
   };
 }
