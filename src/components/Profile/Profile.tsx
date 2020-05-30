81a48d1b8378501b4735a1f9ef284c295b4c32af
bibi1989 <bibiaremieye@gmail.com>
2020-05-30 09:45:57 +0100

    working on error boundary

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import { Table, Tabs, Tab, ListGroup } from "react-bootstrap";
-import {IPayment} from '../../data/models/Payment'
+import { IPayment } from "../../data/models/Payment";
 
 interface Props {
   driver: any;
 }
 
-const Profile: React.FC<Props> = ({driver}) => {
-  console.log({driver}, 'profile')
+const Profile: React.FC<Props> = ({ driver }) => {
+  console.log({ driver }, "profile");
   const payments = driver.payments || [];
 
   return (
-<<<<<<< HEAD
     <Container>
       <TabStyle defaultActiveKey='profile'>
         <Tab eventKey='home' title='QR'>
           <Image>
-            <div className='first'></div>
+            <img src={driver.qrCode} alt='QR CODE' />
           </Image>
         </Tab>
         <Tab eventKey='profile' title='Image'>
@@ -26,83 +25,45 @@ const Profile: React.FC<Props> = ({driver}) => {
           </Image>
         </Tab>
       </TabStyle>
-      {/* <UnOrderList> */}
+      <ListGroup style={{ marginBottom: "20px" }}>
+        <ListGroup.Item>Name: {driver.name}</ListGroup.Item>
+        <ListGroup.Item>Phone Number: {driver.phoneNumber}</ListGroup.Item>
+        <ListGroup.Item>MOT Number: {driver.motNumber}</ListGroup.Item>
+        <ListGroup.Item>Address: {driver.address}</ListGroup.Item>
+        <ListGroup.Item>State: {driver.state}</ListGroup.Item>
+        <ListGroup.Item>Hometown: {driver.hometown}</ListGroup.Item>
+        <ListGroup.Item>
+          Area Of Operation: {driver.areaOfOperation}
+        </ListGroup.Item>
+      </ListGroup>
       <Table responsive>
         <thead>
           <tr>
             <th>Date Time</th>
             <th>Amount</th>
-            {/* <th>Cashier</th> */}
-            <th>MOT Number</th>
+            <th>Cashier</th>
           </tr>
         </thead>
         <tbody>
-          {Array(5)
-            .fill("yes")
-            .map(({ y: string }) => (
-              <tr>
-                <td>Table cell</td>
-                <td>Table cell</td>
-                {/* <td>Table cell</td> */}
-                <td>Table cell</td>
+          {payments.map(
+            ({ createdAt, amount, cashier }: IPayment, index: number) => (
+              <tr key={index}>
+                <td>{createdAt}</td>
+                <td>{amount}</td>
+                <td>{cashier.name}</td>
               </tr>
-            ))}
+            )
+          )}
         </tbody>
       </Table>
-      {/* </UnOrderList> */}
     </Container>
-=======
-    <>
-      <Container>
-        <TabStyle defaultActiveKey='profile'>
-          <Tab eventKey='home' title='QR'>
-            <Image>
-              <img src={driver.qrCode} alt="QR CODE"/>
-            </Image>
-          </Tab>
-          <Tab eventKey='profile' title='Image'>
-            <Image>
-              <div className='second'></div>
-            </Image>
-          </Tab>
-        </TabStyle>
-        <ListGroup style={{marginBottom: '20px'}}>
-          <ListGroup.Item>Name: {driver.name}</ListGroup.Item>
-          <ListGroup.Item>Phone Number: {driver.phoneNumber}</ListGroup.Item>
-          <ListGroup.Item>MOT Number: {driver.motNumber}</ListGroup.Item>
-          <ListGroup.Item>Address: {driver.address}</ListGroup.Item>
-          <ListGroup.Item>State: {driver.state}</ListGroup.Item>
-          <ListGroup.Item>Hometown: {driver.hometown}</ListGroup.Item>
-          <ListGroup.Item>Area Of Operation: {driver.areaOfOperation}</ListGroup.Item>
-        </ListGroup>
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>Date Time</th>
-              <th>Amount</th>
-              <th>Cashier</th>
-            </tr>
-          </thead>
-          <tbody>
-            {payments.map(({createdAt, amount, cashier}: IPayment, index: number) => (
-                <tr key={index}>
-                  <td>{createdAt}</td>
-                  <td>{amount}</td>
-                   <td>{cashier.name}</td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
-      </Container>
-    </>
->>>>>>> d79f888ef7b72319a5047417ec51058c8d12d17e
   );
 };
 
 export default Profile;
 
 export const Container = styled.div`
-  padding: 2% 0;
+  padding: 2% 1em;
   width: 100%;
 `;
 export const TabStyle = styled(Tabs)`
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -14,42 +14,17 @@ const Header: React.FC = () => {
   const history = useHistory();
   return (
     <NavStyle bg='light' expand='lg' sticky='top'>
-      <Navbar.Brand href='#home'>TMS</Navbar.Brand>
+      <Navbar.Brand href='#home'>
+        <Label onClick={() => history.goBack()}>Back</Label>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='mr-auto'>
           <StyleLink to='/report'>Reports</StyleLink>
           <StyleLink to='/defaulter'>Defaulter Report</StyleLink>
-          <Label onClick={() => history.goBack()}>Back</Label>
         </Nav>
       </Navbar.Collapse>
     </NavStyle>
-
-    // <Navbar bg='light' expand='lg'>
-    //   <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-    //   <Navbar.Toggle aria-controls='basic-navbar-nav' />
-    //   <Navbar.Collapse id='basic-navbar-nav'>
-    //     <Nav className='mr-auto'>
-    //       <Nav.Link href='#home'>Home</Nav.Link>
-    //       <Nav.Link href='#link'>Link</Nav.Link>
-    //       <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-    //         <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-    //         <NavDropdown.Item href='#action/3.2'>
-    //           Another action
-    //         </NavDropdown.Item>
-    //         <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-    //         <NavDropdown.Divider />
-    //         <NavDropdown.Item href='#action/3.4'>
-    //           Separated link
-    //         </NavDropdown.Item>
-    //       </NavDropdown>
-    //     </Nav>
-    //     <Form inline>
-    //       <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-    //       <Button variant='outline-success'>Search</Button>
-    //     </Form>
-    //   </Navbar.Collapse>
-    // </Navbar>
   );
 };
 
@@ -70,5 +45,4 @@ const StyleLink = styled(Link)`
 
 const Label = styled.label`
   color: #888;
-  padding: 1em 0;
 `;
